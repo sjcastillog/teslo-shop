eecf6bdace4fa7086e679b609d1d830dbf795b1f
Joao Castillo <joaocastillo91@gmail.com>
2023-03-27 02:32:37 -0500

    first commit new Section

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
 @Entity()
 export class Product {
@@ -48,7 +49,13 @@ export class Product {
         default:[]
     })
     tags: string[];
-    //images
+    
+    @OneToMany(
+        ()=> ProductImage,
+        (productImage) => productImage.product,
+        { cascade:true }
+    )
+    images?:ProductImage[]
 
     @BeforeInsert()
     checkSlugInsert(){
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { validate as isUUID } from 'uuid';
+import { ProductImage } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -15,16 +16,24 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
     
   ){}
 
 
   async create(createProductDto: CreateProductDto) {
     try{
-      const product = this.productRepository.create(createProductDto);
+      const { images = [], ...productDetails } = createProductDto;
+      
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map( image => this.productImageRepository.create({ url:image}))
+      });
+
       await this.productRepository.save( product );
 
-      return product;
+      return {...product, images};
 
     }catch(err){
       this.handleDBExceptions(err);
@@ -67,7 +76,8 @@ export class ProductsService {
   async update( id: string, updateProductDto: UpdateProductDto ) {
     const product  = await this.productRepository.preload({
       id: id,
-      ...updateProductDto
+      ...updateProductDto,
+      images:[]
     })
 
     if(!product) throw new NotFoundException(`Product with id ${id} not found`)
