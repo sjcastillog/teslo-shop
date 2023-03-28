b42e98a87bcf47d2acf16f0137266e79b1538f51
Joao Castillo <joaocastillo91@gmail.com>
2023-03-27 23:19:45 -0500

    27-03

---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,14 @@
+
+export const fileFilter = ( req: Express.Request, file: Express.Multer.File, cb:Function) =>{
+    if( !file) return cb( new Error('File is Empty'), false);
+
+    const fileExtension = file.mimetype.split('/')[1];
+
+    const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+
+    if( validExtensions.includes(fileExtension)){
+        return cb( null, true)
+    }
+
+    cb( null, false);
+}
