6fd88ee59b47d99256d67997a5837fb718bae231
Joao Castillo <joaocastillo91@gmail.com>
2023-03-30 17:35:08 -0500

    last commit 30-03

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,10 @@ import { MessageWsModule } from './message-ws/message-ws.module';
   imports: [ 
     ConfigModule.forRoot(), 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra:process.env.STAGE === 'prod'
+        ? { rejectUnauthorized: false}
+        : null,
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
